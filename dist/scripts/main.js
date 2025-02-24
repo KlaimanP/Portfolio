55daa01a6816299675e8fa4e26ac0a2c018dcd0e
KlaimanP <clement.potteau@gmail.com>
2025-02-24 17:50:34 +0100

    modif

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -5,8 +5,8 @@ let currentWord = "";
 let isDeleting = false;
 const changingText = document.getElementById("changing-text");
 const typingSpeed = 100;
-const deletingSpeed = 30;
-const delayBetweenWords = 1000;
+const deletingSpeed = 50;
+const delayBetweenWords = 1500;
 
 function typeEffect() {
     currentWord = words[index];
